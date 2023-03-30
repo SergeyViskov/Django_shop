7bbbc4e8299680903b0857865daf99a40a1a3802
Пользователь <cocosi383@gmail.com>
2023-03-30 20:31:34 +0300

    gitignore

--- shop/api/views.py
+++ shop/api/views.py
@@ -2,7 +2,6 @@ from rest_framework.response import Response
 from django.shortcuts import get_list_or_404
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
-from rest_framework.pagination import PageNumberPagination
 
 from goods.models import SuperCategory, Goods
 from .serializers import (
